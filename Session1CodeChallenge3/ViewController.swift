8a56698491281034c4fdc7d289688a906c357c8f
RShankar <ravi@rshankar.com>
2015-11-10 16:44:15 +0530

    Initial Version
    Initial Version

--- Session1CodeChallenge3/ViewController.swift
+++ Session1CodeChallenge3/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func calculate(sender: AnyObject) {
+        
+        let equation1 = 5 * 5
+        let equation2 = 4 * 9 * 3
+        let equation3 = 3 * 4 + 5
+        let result = equation1 + equation2 + equation3
+        
+        resultLabel.text = "\(result)"
+    }
 }
 
